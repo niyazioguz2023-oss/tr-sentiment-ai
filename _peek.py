af7564b4fc394c191a22c8b6a5ac7ce9a3c75d8b
Niyazi <niyazioguz2023-oss@users.noreply.github.com>
2026-09-12 21:35:02 +0300

    chore: scratch dosyayi kaldir

--- _peek.py
+++
@@ -1,8 +0,0 @@
-
-from datasets import load_dataset
-ds = load_dataset("winvoker/turkish-sentiment-analysis-dataset")
-print(ds)
-for s in ds:
-    print(s, len(ds[s]), ds[s].features)
-    print(ds[s][:2])
-    break
